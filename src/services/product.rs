ba10ab71d74c03b91ae6cdbfae4280e68d60fde8
Shardul Vaidya <vshardul@amazon.com>
2024-11-11 12:04:10 -0500

    Updated with built-in setup process

--- src/services/product.rs
+++ src/services/product.rs
@@ -5,44 +5,6 @@ use aws_sdk_dynamodb::types::AttributeValue;
 use rocket::serde::json::Json;
 use rocket::{error, get, post, State};
 
-#[get("/q/product/search/<val>")]
-pub async fn q_search_product(
-    val: &str,
-    db: &State<ddb::Client>,
-    table_name: &State<String>,
-) -> UIResponder<Vec<Product>> {
-    println!("Incoming request value: {:?}",  val);
-
-    let cleaned_val = val.trim_matches('"');
-
-    let results = db
-        .query()
-        .table_name(table_name.to_string())
-        .key_condition_expression("partition_key = :pk_val")
-        .expression_attribute_values(":pk_val", AttributeValue::S("PRODUCT".to_string()))
-        .filter_expression("contains(description, :name)")
-        .expression_attribute_values(
-            ":name",
-            AttributeValue::S(cleaned_val.to_string()),
-        )
-        .send()
-        .await;
-
-    match results {
-        Ok(res) => match reconstruct_results::<Product>(res) {
-            Ok(res) => {
-                println!("{:?}", res);
-                UIResponder::Ok(Json::from(res))
-            }
-            Err(err) => UIResponder::Err(error!("{:?}", err)),
-        },
-        Err(err) => {
-            println!("{:?}", err);
-            UIResponder::Err(error!("Something went wrong"))
-        }
-    }
-}
-
 #[get("/product/<id>")]
 pub async fn get_product(
     id: &str,
---
+++ src/services/q.rs
@@ -0,0 +1,44 @@
+use aws_sdk_dynamodb::types::AttributeValue;
+use rocket::{error, get, State};
+use aws_sdk_dynamodb as ddb;
+use rocket::serde::json::Json;
+use crate::types::{Product, UIResponder};
+use crate::utils::reconstruct_results;
+
+#[get("/q/product/search/<val>")]
+pub async fn q_search_product(
+    val: &str,
+    db: &State<ddb::Client>,
+    table_name: &State<String>,
+) -> UIResponder<Vec<Product>> {
+    println!("Incoming request value: {:?}",  val);
+
+    let cleaned_val = val.trim_matches('"');
+
+    let results = db
+        .query()
+        .table_name(table_name.to_string())
+        .key_condition_expression("partition_key = :pk_val")
+        .expression_attribute_values(":pk_val", AttributeValue::S("PRODUCT".to_string()))
+        .filter_expression("contains(description, :name)")
+        .expression_attribute_values(
+            ":name",
+            AttributeValue::S(cleaned_val.to_string()),
+        )
+        .send()
+        .await;
+
+    match results {
+        Ok(res) => match reconstruct_results::<Product>(res) {
+            Ok(res) => {
+                println!("{:?}", res);
+                UIResponder::Ok(Json::from(res))
+            }
+            Err(err) => UIResponder::Err(error!("{:?}", err)),
+        },
+        Err(err) => {
+            println!("{:?}", err);
+            UIResponder::Err(error!("Something went wrong"))
+        }
+    }
+}
